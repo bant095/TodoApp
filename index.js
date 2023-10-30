604cff7b2e49addd4b457e067a9143a122f682f1
Abolaji Ayodeji <abolajiayodeji93@gmail.com>
2023-10-30 12:38:53 +0100

    I updated my project

---
+++ index.js
@@ -0,0 +1,27 @@
+//Utility function
+function broofa(){
+    return ''
+}
+
+//TODO: CRUD FUNCTIONS
+
+//CREATE TODO FUNCTION
+
+/*
+1. Get todo from user input
+2. Add todo to local storage / API
+*/
+
+const createTodo = () => {
+    const todo = document.querySelector('#todo-input')
+    const newTodo = {
+        id: ''
+        title: todoInput.value
+    }
+}
+
+//READ TODO FUNCTION
+
+//UPDATE TODO FUNCTION
+
+// DELETE TODO FUNCTION
